5fe27eb3abf2ebebbe8d2adcef4f88204af4f371
vitvikont <vitvikont@mail.ru>
2026-05-05 14:27:44 +0300

    first commit: начал изучение алгоритмов

---
+++ lession_binary_search_algorithm.py
@@ -0,0 +1,23 @@
+array = [23, 12, 14, 32, 19, 74, 93, 8, 103, 24, 54]
+
+
+def binary_search(arr, search):
+    # Алгоритм бинарного поиска в массиве
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        middle = (left + right) // 2
+        if arr[middle] < search:
+            left = middle + 1  # Ищем в правой половине
+        elif arr[middle] > search:
+            right = middle - 1  # Ищем в левой половине
+        else:
+            return middle  # Элемент найден, вернуть индекс
+    return -1  # Элемент не найден
+
+
+if __name__ == '__main__':
+    value = 101
+    result = binary_search(array, value)
+    print(result)
